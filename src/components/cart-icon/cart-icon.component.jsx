7b91ab199d7b309b179d859a6f56aeff24f78676
Luminus Olumide Alabi <luminus@vedutanova.com>
2021-12-28 12:03:00 +0000

    update shop data, also update cart utils to group items in cart reducer

--- src/components/cart-icon/cart-icon.component.jsx
+++ src/components/cart-icon/cart-icon.component.jsx
@@ -7,7 +7,7 @@ import { toggleCartHidden } from '../../redux/cart/cart.actions'
 const CartIcon = ({ toggleCartHidden }) => (
   <div className="cart-icon" onClick={toggleCartHidden}>
     <ShoppingIcon className="shopping-icon"/ >
-    <span className="item-count"></span>
+    <span className="item-count">0</span>
   </div>
 )
 
