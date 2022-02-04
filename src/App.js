6ce801731532238edf80b1489a96526bdc6928d5
Luminus Olumide Alabi <luminus@vedutanova.com>
2022-02-04 08:54:27 +0000

    pull shop data from firestore into the shop reducer from the shop page and Implement WithSpinner HOC

--- src/App.js
+++ src/App.js
@@ -14,6 +14,7 @@ import { setCurrentUser } from './redux/user/user.actions'
 import { createStructuredSelector } from 'reselect'
 import { selectCurrentUser } from './redux/user/user.selectors'
 
+
 class App extends React.Component {
 
   unsubscribeFromAuth = null
--- src/firebase/firebase.utils.js
+++ src/firebase/firebase.utils.js
@@ -16,8 +16,12 @@ export const createUserProfileDocument = async (userAuth, additionalData) => {
   if (!userAuth) return
   
   const userRef = firestore.doc(`users/${userAuth.uid}`)
+  const collectionRef = firestore.collection('users')
+
   const snapShot = await userRef.get()
-  
+  const collectionSnapshot = await collectionRef.get()
+  console.log({ collection: collectionSnapshot.docs.map(doc => doc.data()) });
+
   if (!snapShot.exists) {
     const { displayName, email } = userAuth
     const createdAt = new Date()
@@ -41,6 +45,37 @@ firebase.initializeApp(config)
 
 export const auth = firebase.auth()
 export const firestore = firebase.firestore()
+
+export const addCollectionAndDocuments = async (collectionKey, objectsToAdd) => {
+  const collectionRef = firestore.collection(collectionKey)
+  console.log(collectionRef)
+
+  const batch = firestore.batch()
+  objectsToAdd.forEach(object => {
+    const newDocRef = collectionRef.doc()
+    batch.set(newDocRef, object)
+  })
+
+  return await batch.commit()
+}
+
+export const convertCollectionsSnapshotToMap = (collections) => {
+  const transformedCollection = collections.docs.map(doc => {
+    const { title, items } = doc.data()
+
+    return {
+      routeName: encodeURI(title.toLowerCase()),
+      id: doc.id,
+      title,
+      items
+    }
+  })
+
+  return transformedCollection.reduce((accumulator, collection) => {
+    accumulator[collection.title.toLowerCase()] = collection
+    return accumulator
+  }, {})
+}
 
 const provider = new firebase.auth.GoogleAuthProvider()
 provider.setCustomParameters({ prompt: 'select_account' })
